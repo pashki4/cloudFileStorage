b31ce86566ad678b5ffa86914584bc97dd547a20
PashaVyshniakov <pashki4@gmail.com>
2024-03-31 21:35:36 +0300

    reworking tests

--- src/test/java/dev/pasha/cloudfilestorage/repository/UserRepositoryTest.java
+++ src/test/java/dev/pasha/cloudfilestorage/repository/UserRepositoryTest.java
@@ -4,9 +4,12 @@ import dev.pasha.cloudfilestorage.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -34,22 +37,42 @@ class UserRepositoryTest {
         assertThat(container.isRunning()).isTrue();
     }
 
-    @BeforeEach
-    void setup() {
-        List<User> users = List.of(new User("newUser", "password"),
-                new User("newUser2", "password"));
-        userRepository.saveAll(users);
+    @Test
+    void shouldReturnUserByUserName() {
+        //Given
+        User user = new User("newUser", "password");
+        userRepository.save(user);
+
+        //When
+        Optional<User> actual = userRepository.findByUsername(user.getUsername());
+
+        //Then
+        assertThat(actual).isPresent();
     }
 
     @Test
-    void shouldReturnUserByUserName() {
-        Optional<User> user = userRepository.findByUsername("newUser");
-        assertThat(user.isPresent()).isTrue();
+    void shouldNotReturnUserWhenNameDoesNotExist() {
+        //Given
+        String name = "alex";
+
+        //When
+        Optional<User> actual = userRepository.findByUsername(name);
+
+        //Then
+        assertThat(actual).isNotPresent();
     }
 
     @Test
     void shouldReturnCountOfUsers() {
-        List<User> users = userRepository.findAll();
-        assertThat(users.size()).isEqualTo(2);
+        //Given
+        User user = new User("user", "password");
+        userRepository.save(user);
+
+        //When
+        Optional<User> actual = userRepository.findAll().stream()
+                .filter(u -> u.getUsername().equals(user.getUsername()))
+                .findAny();
+        //Then
+        assertThat(actual).isPresent().hasValue(user);
     }
 }
